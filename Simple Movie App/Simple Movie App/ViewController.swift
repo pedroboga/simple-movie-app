72713faf1fa30032de2fbd5a5bae5c07b088c815
pedroboga <pedroboga@gmail.com>
2021-10-25 07:05:23 -0300

    Criado projeto no xcode

---
+++ Simple Movie App/Simple Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Movie App
+//
+//  Created by Pedro Boga on 25/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
